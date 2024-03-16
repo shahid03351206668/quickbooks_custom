c34646c756b10b9701e60deacbe5c5c832e58f9b
Malik M. Shahid Khan <shahid03351206668@gmail.com>
2024-03-17 00:36:20 +0500

    fixes 12

--- quickbooks_custom/custom_quickbooks_migrator.py
+++ quickbooks_custom/custom_quickbooks_migrator.py
@@ -538,6 +538,9 @@ class QuickBooksMigratorCustom(Document):
 
 	def _save_customer(self, customer):
 		try:
+			customer_name = encode_company_abbr(customer["DisplayName"], self.company)
+			customer_name = str(customer_name).replace(">", "")
+			customer_name = str(customer_name).replace("<", "")
 			if not frappe.db.exists(
 				{"doctype": "Customer", "quickbooks_id": customer["Id"], "company": self.company}
 			):
@@ -552,16 +555,11 @@ class QuickBooksMigratorCustom(Document):
 					)[0]["name"]
 				except Exception:
 					receivable_account = None
-
-				# customer_name = encode_company_abbr(customer["DisplayName"], self.company)
-				# customer_name = str(customer_name).replace(">", "")
-				# customer_name = str(customer_name).replace("<", "")
-
 				erpcustomer = frappe.get_doc(
 					{
 						"doctype": "Customer",
 						"quickbooks_id": customer["Id"],
-						"customer_name": encode_company_abbr(customer["DisplayName"], self.company),
+						"customer_name": customer_name,
 						"customer_type": "Individual",
 						"customer_group": "Commercial",
 						"default_currency": customer["CurrencyRef"]["value"],
