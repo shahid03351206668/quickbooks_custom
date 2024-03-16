d04e80f8ff8e78dabd300409ad98a11c3c68de67
Malik M. Shahid Khan <shahid03351206668@gmail.com>
2024-03-16 22:30:08 +0500

    fixes 4

--- quickbooks_custom/custom_quickbooks_migrator.py
+++ quickbooks_custom/custom_quickbooks_migrator.py
@@ -21,5 +21,64 @@ def create_address(self, entity, doctype, address, address_type):
 	except Exception as e:
 		self._log_error(e, address)
 
+def _save_vendor(self, vendor):
+	try:
+		if not frappe.db.exists(
+			{"doctype": "Supplier", "quickbooks_id": vendor["Id"], "company": self.company}
+		):
+			erpsupplier = frappe.get_doc(
+				{
+					"doctype": "Supplier",
+					"quickbooks_id": vendor["Id"],
+					"supplier_name": encode_company_abbr(vendor["DisplayName"], self.company),
+					"supplier_group": "All Supplier Groups",
+					"company": self.company,
+				}
+			).insert()
+			if "BillAddr" in vendor:
+				create_address(erpsupplier, "Supplier", vendor["BillAddr"], "Billing")
+			if "ShipAddr" in vendor:
+				create_address(erpsupplier, "Supplier", vendor["ShipAddr"], "Shipping")
+	except Exception as e:
+		self._log_error(e)
 
-QuickBooksMigrator._create_address = create_address
+def _save_customer(self, customer):
+	try:
+		if not frappe.db.exists(
+			{"doctype": "Customer", "quickbooks_id": customer["Id"], "company": self.company}
+		):
+			try:
+				receivable_account = frappe.get_all(
+					"Account",
+					filters={
+						"account_type": "Receivable",
+						"account_currency": customer["CurrencyRef"]["value"],
+						"company": self.company,
+					},
+				)[0]["name"]
+			except Exception:
+				receivable_account = None
+			erpcustomer = frappe.get_doc(
+				{
+					"doctype": "Customer",
+					"quickbooks_id": customer["Id"],
+					"customer_name": encode_company_abbr(customer["DisplayName"], self.company),
+					"customer_type": "Individual",
+					"customer_group": "Commercial",
+					"default_currency": customer["CurrencyRef"]["value"],
+					"accounts": [{"company": self.company, "account": receivable_account}],
+					"territory": "All Territories",
+					"company": self.company,
+				}
+			).insert()
+			if "BillAddr" in customer:
+				create_address(erpcustomer, "Customer", customer["BillAddr"], "Billing")
+			if "ShipAddr" in customer:
+				create_address(erpcustomer, "Customer", customer["ShipAddr"], "Shipping")
+	except Exception as e:
+		self._log_error(e, customer)
+
+
+# QuickBooksMigrator.create_address = create_address
+QuickBooksMigrator._save_vendor = _save_vendor
+QuickBooksMigrator._save_customer = _save_customer
