2f73cecbb1ef7023df203b369759e1096d0dd69f
Malik M. Shahid Khan <shahid03351206668@gmail.com>
2024-03-16 21:51:36 +0500

    fixes

--- quickbooks_custom/custom_quickbooks_migrator.py
+++ quickbooks_custom/custom_quickbooks_migrator.py
@@ -3,23 +3,22 @@
 import frappe
 from frappe import _
 from erpnext.erpnext_integrations.doctype.quickbooks_migrator.quickbooks_migrator import QuickBooksMigrator
+from frappe.model.document import Document
 
-def _create_address(self, entity, doctype, address, address_type):
-	try:
-		if not frappe.db.exists({"doctype": "Address", "quickbooks_id": address.get("Id")}):
-			frappe.get_doc(
-				{
-					"doctype": "Address",
-					"quickbooks_address_id": address.get("Id"),
-					"address_title": entity.name,
-					"address_type": address_type,
-					"address_line1": address.get("Line1"),
-					"city": address.get("City"),
-					"links": [{"link_doctype": doctype, "link_name": entity.name}]
-				}
-			).insert()
-	except Exception as e:
-		self._log_error(e, address)
-
-
-QuickBooksMigrator._create_address = _create_address
+class QuickBooksMigratorCustom(Document):
+	def _create_address(self, entity, doctype, address, address_type):
+		try:
+			if not frappe.db.exists({"doctype": "Address", "quickbooks_id": address.get("Id")}):
+				frappe.get_doc(
+					{
+						"doctype": "Address",
+						"quickbooks_address_id": address.get("Id"),
+						"address_title": entity.name,
+						"address_type": address_type,
+						"address_line1": address.get("Line1"),
+						"city": address.get("City"),
+						"links": [{"link_doctype": doctype, "link_name": entity.name}]
+					}
+				).insert()
+		except Exception as e:
+			self._log_error(e, address)
